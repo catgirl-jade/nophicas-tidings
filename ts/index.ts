e074aa88f494390c9bde05dd4aa1f97ebaa38047
catgirl-jade <catgirl-jade@users.noreply.github.com>
2024-06-12 21:10:51 -0400

    CHanged license so it shows up properly

--- ts/index.ts
+++ ts/index.ts
@@ -1,12 +1,14 @@
-// Copyright 2023 catgirl-jade
-// 
-// This file is part of nophicas-tidings.
-// 
-// nophicas-tidings is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// 
-// nophicas-tidings is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License along with nophicas-tidings. If not, see <https://www.gnu.org/licenses/>.
+/*! 
+ * Copyright 2023 catgirl-jade
+ * 
+ * This file is part of nophicas-tidings.
+ * 
+ * nophicas-tidings is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * nophicas-tidings is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with nophicas-tidings. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 /// Imports
 import { WorkerData } from "./common";
